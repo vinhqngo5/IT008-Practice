f1bf4cd399d29e57d5c1e6c83a1765f139477f5e
Manh Cuong <cuongnguyen14022001@gmail.com>
2021-09-21 13:06:36 +0700

    Add sql file for data and Winform application

---
+++ QuanLyQuanCafe/QuanLyQuanCafe1/fAdmin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyQuanCafe1
+{
+    public partial class fAdmin : Form
+    {
+        public fAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void tcBill_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tpTable_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView3_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void panel14_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel18_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
