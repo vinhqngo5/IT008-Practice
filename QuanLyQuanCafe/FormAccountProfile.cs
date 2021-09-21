31b800f2c5de8c70e62d3993fec181aaf3eab1d5
lehuutrung1412 <lehuutrung1412@users.noreply.github.com>
2021-09-21 20:38:47 +0700

    Add form account profile

---
+++ QuanLyQuanCafe/FormAccountProfile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyQuanCafe
+{
+    public partial class FormAccountProfile : Form
+    {
+        public FormAccountProfile()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Cập nhật thành công!", "Cập nhật");
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
