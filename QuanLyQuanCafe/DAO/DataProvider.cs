5287b345e92a59cfe307b5a4d826a998df3e0d42
lehuutrung1412 <lehuutrung1412@users.noreply.github.com>
2021-09-23 16:39:32 +0700

    Add DataAccessObject/DataProvider

---
+++ QuanLyQuanCafe/DAO/DataProvider.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyQuanCafe.DAO
+{
+    public class DataProvider
+    {
+        private string _connectionStr = "Data Source=.\\;Initial Catalog=QUANLYQUANCAFE;Integrated Security=True";
+        
+        public DataTable ExecuteQuery(string query, object[] parameter = null)
+        {
+            DataTable data = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(_connectionStr)) {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                if (parameter != null)
+                {
+                    string[] listQueryString = query.Split(' ');
+                    int i = 0;
+                    foreach (string queryItem in listQueryString)
+                    {
+                        if (queryItem.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(queryItem, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+                adapter.Fill(data);
+
+                connection.Close();
+            }
+
+            return data;
+        }
+
+        public int ExecuteNonQuery(string query, object[] parameter = null)
+        {
+            int data = 0;
+
+            using (SqlConnection connection = new SqlConnection(_connectionStr))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                if (parameter != null)
+                {
+                    string[] listQueryString = query.Split(' ');
+                    int i = 0;
+                    foreach (string queryItem in listQueryString)
+                    {
+                        if (queryItem.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(queryItem, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+
+                data = command.ExecuteNonQuery();
+
+                connection.Close();
+            }
+
+            return data;
+        }
+
+        public object ExecuteScalar(string query, object[] parameter = null)
+        {
+            object data = new object();
+
+            using (SqlConnection connection = new SqlConnection(_connectionStr))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                if (parameter != null)
+                {
+                    string[] listQueryString = query.Split(' ');
+                    int i = 0;
+                    foreach (string queryItem in listQueryString)
+                    {
+                        if (queryItem.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(queryItem, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+
+                data = command.ExecuteScalar();
+
+                connection.Close();
+            }
+
+            return data;
+        }
+    }
+}
