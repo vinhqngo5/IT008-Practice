0b9b23f943d9f1c07c6a7c793e8bca5e8a2b1cd1
Manh Cuong <cuongnguyen14022001@gmail.com>
2021-09-23 21:29:11 +0700

    Add using *.DAO

--- QuanLyQuanCafe/DAO/DataProvider.cs
+++ QuanLyQuanCafe/DAO/DataProvider.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 
-namespace QuanLyQuanCafe
+namespace QuanLyQuanCafe.DAO
 {
     public class DataProvider
     {
