ad7889081783b3f843f1fa392329078d78ccc74f
lehuutrung1412 <lehuutrung1412@users.noreply.github.com>
2021-09-26 14:00:00 +0700

    Add bill info DAO

---
+++ QuanLyQuanCafe/DAO/BillInfoDAO.cs
@@ -0,0 +1,36 @@
+﻿using QuanLyQuanCafe.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyQuanCafe.DAO
+{
+    public class BillInfoDAO
+    {
+        private static BillInfoDAO s_instance;
+
+        public static BillInfoDAO Instance 
+        { 
+            get => s_instance ?? (s_instance = new BillInfoDAO()); 
+            private set => s_instance = value; 
+        }
+
+        public List<BillInfo> GetListBillInfo(int idBill)
+        {
+            DataTable dataBillInfo = DataProvider.Instance.ExecuteQuery
+                        (
+                         "EXEC USP_GetBillInfo @idBill",
+                         new object[] { idBill }
+                        );
+            List<BillInfo> listBillInfo = new List<BillInfo>();
+            foreach (DataRow row in dataBillInfo.Rows)
+            {
+                listBillInfo.Add(new BillInfo(row));
+            }
+            return listBillInfo;
+        }
+    }
+}
