aa260eb6941b839baa1a1f53a2d8c93da78b0850
lehuutrung1412 <lehuutrung1412@users.noreply.github.com>
2021-09-26 14:00:00 +0700

    Update form table manager

--- QuanLyQuanCafe/FormTableManager.cs
+++ QuanLyQuanCafe/FormTableManager.cs
@@ -28,19 +28,46 @@ namespace QuanLyQuanCafe
             List<Table> tableList = TableDAO.Instance.LoadTableList();
             foreach (Table table in tableList)
             {
-                flpTable.Controls.Add(new Button()
+                Button btnTable = new Button()
                 {
                     Width = TableDAO.TableWidth,
                     Height = TableDAO.TableHeight,
                     Text = table.Name + "\n" + (table.Status ? "Có người" : "Trống"),
-                    BackColor = table.Status ? Color.LightPink : Color.LightGreen
-                });
+                    BackColor = table.Status ? Color.LightPink : Color.LightGreen,
+                    Tag = table,
+
+                };
+
+                btnTable.Click += BtnTable_Click;
+
+                flpTable.Controls.Add(btnTable);
             }
+        }
+
+        void ShowBill(int idTable)
+        {
+            lsvBill.Items.Clear();
+            List<DTO.Menu> listMenu = MenuDAO.Instance.GetListMenuByTable(idTable);
+            foreach (DTO.Menu item in listMenu)
+            {
+                ListViewItem lsvItem = new ListViewItem(item.FoodName);
+                lsvItem.SubItems.Add(item.Count.ToString());
+                lsvItem.SubItems.Add(item.Price.ToString());
+                lsvItem.SubItems.Add(item.TotalPrice.ToString());
+                lsvBill.Items.Add(lsvItem);
+            }
+
         }
 
         #endregion
 
         #region Events
+
+        private void BtnTable_Click(object sender, EventArgs e)
+        {
+            ShowBill(((sender as Button).Tag as Table).Id);
+        }
+
         private void TsmiLogout_Click(object sender, EventArgs e)
         {
             this.Close();
