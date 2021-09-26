98c2eb94c2e67baade2cc6ffb366e837fe8219ed
lehuutrung1412 <lehuutrung1412@users.noreply.github.com>
2021-09-26 14:00:00 +0700

    Add Bill, bill info, menu DTO

---
+++ QuanLyQuanCafe/DTO/Bill.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyQuanCafe.DTO
+{
+    public class Bill
+    {
+        private int _id;
+        private DateTime? _dateCheckIn;
+        private DateTime? _dateCheckOut;
+        private int _idTable;
+        private bool _status;
+
+        public Bill(DataRow row)
+        {
+            this.Id = Convert.ToInt32(row["Id"]);
+            this.DateCheckIn = (DateTime?)row["DateCheckIn"];
+            if (row["DateCheckOut"].ToString() != "")
+            {
+                this.DateCheckOut = (DateTime?)row["DateCheckOut"];
+            }
+            else
+            {
+                this.DateCheckOut = null;
+            }
+            this.IdTable = Convert.ToInt32(row["IdTable"]);
+            this.Status = Convert.ToBoolean(row["Status"]);
+        }
+
+        public int Id { get => _id; set => _id = value; }
+        public DateTime? DateCheckIn { get => _dateCheckIn; set => _dateCheckIn = value; }
+        public DateTime? DateCheckOut { get => _dateCheckOut; set => _dateCheckOut = value; }
+        public int IdTable { get => _idTable; set => _idTable = value; }
+        public bool Status { get => _status; set => _status = value; }
+    }
+}
---
+++ QuanLyQuanCafe/DTO/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyQuanCafe.DTO
+{
+    public class Menu
+    {
+        private string _foodName;
+        private int _count;
+        private float _price;
+        private float _totalPrice;
+
+        public string FoodName { get => _foodName; set => _foodName = value; }
+        public int Count { get => _count; set => _count = value; }
+        public float Price { get => _price; set => _price = value; }
+        public float TotalPrice { get => _totalPrice; set => _totalPrice = value; }
+
+        public Menu(DataRow row)
+        {
+            this.FoodName = Convert.ToString(row["Name"]);
+            this.Price = Convert.ToSingle(row["Price"]);
+            this.Count = Convert.ToInt32(row["Count"]);
+            this.TotalPrice = Convert.ToSingle(row["TotalPrice"]);
+        }
+    }
+}
