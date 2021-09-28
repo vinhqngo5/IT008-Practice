d14c144d6b890cc6583555b00dc6259136a434c6
lehuutrung1412 <lehuutrung1412@users.noreply.github.com>
2021-09-28 11:11:08 +0700

    Add category

---
+++ QuanLyQuanCafe/DTO/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyQuanCafe.DTO
+{
+    public class Category
+    {
+        private int _id;
+        private string _name;
+
+        public Category(DataRow row)
+        {
+            this.Id = Convert.ToInt32(row["Id"]);
+            this.Name = Convert.ToString(row["Name"]);
+        }
+
+        public int Id { get => _id; set => _id = value; }
+        public string Name { get => _name; set => _name = value; }
+    }
+}
