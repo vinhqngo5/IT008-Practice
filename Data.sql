8f646143fb2c79d92d1c6c0c88400ff7b22d8cec
lehuutrung1412 <lehuutrung1412@users.noreply.github.com>
2021-10-04 17:37:41 +0700

    Update sql search food

--- Data.sql
+++ Data.sql
@@ -486,6 +486,16 @@ BEGIN
 END
 GO
 
+CREATE PROC USP_SearchFood
+	@name NVARCHAR(100)
+AS
+BEGIN
+	SELECT * 
+	FROM dbo.Food 
+	WHERE dbo.fuConvertToUnsign1(name) LIKE '%' + dbo.fuConvertToUnsign1(@name) + '%'
+END
+GO
+
 -- TRIGGER --
 CREATE TRIGGER UTG_UpdateBillInfo
 ON dbo.BillInfo FOR INSERT, UPDATE
@@ -573,3 +583,39 @@ BEGIN
 	ON TableFood.Id = t.Id
 END
 GO
+
+-- FUNCTION
+CREATE FUNCTION [dbo].[fuConvertToUnsign1] ( @strInput NVARCHAR(4000) ) 
+RETURNS NVARCHAR(4000) 
+AS 
+BEGIN 
+	IF @strInput IS NULL RETURN @strInput 
+	IF @strInput = '' RETURN @strInput 
+	DECLARE @RT NVARCHAR(4000) 
+	DECLARE @SIGN_CHARS NCHAR(136) 
+	DECLARE @UNSIGN_CHARS NCHAR (136) 
+	SET @SIGN_CHARS = N'ăâđêôơưàảãạáằẳẵặắầẩẫậấèẻẽẹéềểễệế ìỉĩịíòỏõọóồổỗộốờởỡợớùủũụúừửữựứỳỷỹỵý ĂÂĐÊÔƠƯÀẢÃẠÁẰẲẴẶẮẦẨẪẬẤÈẺẼẸÉỀỂỄỆẾÌỈĨỊÍ ÒỎÕỌÓỒỔỖỘỐỜỞỠỢỚÙỦŨỤÚỪỬỮỰỨỲỶỸỴÝ' +NCHAR(272)+ NCHAR(208) 
+	SET @UNSIGN_CHARS = N'aadeoouaaaaaaaaaaaaaaaeeeeeeeeee iiiiiooooooooooooooouuuuuuuuuuyyyyy AADEOOUAAAAAAAAAAAAAAAEEEEEEEEEEIIIII OOOOOOOOOOOOOOOUUUUUUUUUUYYYYYDD' 
+	DECLARE @COUNTER int 
+	DECLARE @COUNTER1 int 
+	SET @COUNTER = 1 
+	WHILE (@COUNTER <=LEN(@strInput)) 
+	BEGIN 
+		SET @COUNTER1 = 1 
+		WHILE (@COUNTER1 <=LEN(@SIGN_CHARS)+1) 
+			BEGIN 
+				IF UNICODE(SUBSTRING(@SIGN_CHARS, @COUNTER1,1)) = UNICODE(SUBSTRING(@strInput,@COUNTER ,1) ) 
+					BEGIN 
+						IF @COUNTER=1 
+							SET @strInput = SUBSTRING(@UNSIGN_CHARS, @COUNTER1,1) + SUBSTRING(@strInput, @COUNTER+1,LEN(@strInput)-1) 
+						ELSE 
+							SET @strInput = SUBSTRING(@strInput, 1, @COUNTER-1) +SUBSTRING(@UNSIGN_CHARS, @COUNTER1,1) + SUBSTRING(@strInput, @COUNTER+1,LEN(@strInput)- @COUNTER) 
+						BREAK 
+					END 
+				SET @COUNTER1 = @COUNTER1 +1 
+			END 
+		SET @COUNTER = @COUNTER +1 
+	END 
+	SET @strInput = replace(@strInput,' ','-') 
+	RETURN @strInput 
+END
