bf8d5be22d9b4e97cd1f49deffabd83469e8ecec
Manh Cuong <cuongnguyen14022001@gmail.com>
2021-10-08 19:29:50 +0700

    Create Login UI

---
+++ QuanLyKho/QuanLyKho/ViewModel/ControlBarViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace QuanLyKho.ViewModel
+{
+    public class ControlBarViewModel : BaseViewModel
+    {
+        #region commands
+        public ICommand CloseWindowCommand { get; set; }
+        public ICommand MaximizeWindowCommand { get; set; }
+        public ICommand MinimizeWindowCommand { get; set; }
+        public ICommand MouseMoveCommand { get; set; }
+        #endregion
+        public ControlBarViewModel()
+        {
+            CloseWindowCommand = new RelayCommand<UserControl>((p)=> { return p==null? false:true; },(p)=> { 
+                Window window = GetWindowParent(p);
+                if (window != null)
+                    window.Close();
+            })  ;
+            MaximizeWindowCommand = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) => {
+                Window window = GetWindowParent(p);
+                if (window != null)
+                {
+                    if (window.WindowState != WindowState.Maximized)
+                        window.WindowState = WindowState.Maximized;
+                    else
+                        window.WindowState = WindowState.Normal;
+                }    
+            });
+            MinimizeWindowCommand = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) => {
+                Window window = GetWindowParent(p);
+                if (window != null)
+                {
+                    if (window.WindowState != WindowState.Minimized)
+                        window.WindowState = WindowState.Minimized;
+                }    
+            });
+            MouseMoveCommand = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) => {
+                Window window = GetWindowParent(p);
+                if (window != null)
+                {
+                    if (window.WindowState != WindowState.Minimized)
+                        window.DragMove();
+                }
+            });
+        }
+        Window GetWindowParent(UserControl p)
+        {
+            FrameworkElement element = p;
+            while (element.Parent != null)
+                element = element.Parent as FrameworkElement;
+            return element as Window;
+        }
+    }
+}
---
+++ QuanLyKho/QuanLyKho/ViewModel/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace QuanLyKho.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        // mọi thứ xử lý sẽ nằm trong này
+        public bool isLoaded = false;
+        public ICommand LoadedWindowCommand { get; set; }
+        public MainViewModel()
+        {
+            if(isLoaded==false)
+            {
+                LoadedWindowCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
+                    isLoaded = true;
+                    LoginWindow loginWindow = new LoginWindow();
+                    loginWindow.ShowDialog();
+                });
+            }    
+        }
+    }
+}
