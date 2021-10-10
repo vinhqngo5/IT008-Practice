3de139adca3a062b89d5e48a95c10eedb363ac5e
vinhqngo5@gmail.com <vinhqngo5@gmail.com>
2021-10-10 18:26:32 +0700

    Create control bar for app

--- QuanLyKho/ViewModel/MainViewModel.cs
+++ QuanLyKho/ViewModel/MainViewModel.cs
@@ -3,24 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace QuanLyKho.ViewModel
 {
     class MainViewModel : BaseViewModel
     {
         private bool _isLoaded = false;
+        public ICommand LoadedWindowCommand { get; set; }
+        public bool IsLoaded { get => _isLoaded; set => _isLoaded = value; }
+
         public MainViewModel()
         {
-            LoginWindow loginWindow = new LoginWindow();
 
-            if (!this.IsLoaded)
+            LoadedWindowCommand = new RelayCommand<object>(p => true, p =>
             {
-                this.IsLoaded = true;
+                IsLoaded = true;
+                LoginWindow loginWindow = new LoginWindow();
                 loginWindow.ShowDialog();
-            }
+            });
 
         }
 
-        public bool IsLoaded { get => _isLoaded; set => _isLoaded = value; }
     }
 }
