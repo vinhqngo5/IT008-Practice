b071921e79d9d0d9673f2f8f3d48b2bd0e2a1291
19521178 <lthyntat>
2021-10-10 15:48:28 +0700

    Implement toolbar

--- QuanLyKho/MainWindow.xaml.cs
+++ QuanLyKho/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace QuanLyKho
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
--- QuanLyKho/ViewModel/MainViewModel.cs
+++ QuanLyKho/ViewModel/MainViewModel.cs
@@ -4,21 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace QuanLyKho.ViewModel
 {
   class MainViewModel : BaseViewModel
   {
     private bool _isLoaded = false;
+    public ICommand LoadedWindowCommand { get; set; }
     public MainViewModel()
     {
-      LoginWindow loginWindow = new LoginWindow();
-
-      if (!this.IsLoaded)
-      {
-        this.IsLoaded = true;
-        loginWindow.ShowDialog();
-      }
+        LoginWindow loginWindow = new LoginWindow();
+        LoadedWindowCommand = new RelayCommand<UserControl>((p) => { return true; }, (p) =>
+        {
+            this.IsLoaded = true;
+            loginWindow.ShowDialog();
+        });
 
     }
 
