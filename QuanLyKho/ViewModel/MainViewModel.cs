c566977535dd54cbb28a0958fc36234669ca8b38
19521178 <lthyntat>
2021-10-10 23:23:41 +0700

    Create interface of Add, Edit, Delete Windows

--- QuanLyKho/ViewModel/MainViewModel.cs
+++ QuanLyKho/ViewModel/MainViewModel.cs
@@ -11,19 +11,62 @@ namespace QuanLyKho.ViewModel
 {
   class MainViewModel : BaseViewModel
   {
-    private bool _isLoaded = false;
-    public ICommand LoadedWindowCommand { get; set; }
-    public MainViewModel()
-    {
-        LoginWindow loginWindow = new LoginWindow();
-        LoadedWindowCommand = new RelayCommand<UserControl>((p) => { return true; }, (p) =>
+        private bool _isLoaded = false;
+        public ICommand LoadedWindowCommand { get; set; }
+        public ICommand UnitCommand { get; set; }
+        public ICommand SupplierCommand { get; set; }
+        public ICommand CustomerCommand { get; set; }
+        public ICommand ObjectCommand { get; set; }
+        public ICommand UserCommand { get; set; }
+        public ICommand InputCommand { get; set; }
+        public ICommand OutputCommand { get; set; }
+        public MainViewModel()
         {
-            this.IsLoaded = true;
-            loginWindow.ShowDialog();
-        });
+            
+            LoadedWindowCommand = new RelayCommand<UserControl>((p) => { return true; }, (p) =>
+            {
+                this.IsLoaded = true;
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.ShowDialog();
+            });
+            UnitCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                UnitWindow wd = new UnitWindow();
+                wd.ShowDialog();
+            });
+            SupplierCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                SupplierWindow wd = new SupplierWindow();
+                wd.ShowDialog();
+            });
+            CustomerCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                CustomerWindow wd = new CustomerWindow();
+                wd.ShowDialog();
+            });
+            ObjectCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                ObjectWindow wd = new ObjectWindow();
+                wd.ShowDialog();
+            });
+            UserCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                UserWindow wd = new UserWindow();
+                wd.ShowDialog();
+            });
+            InputCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                InputWindow wd = new InputWindow();
+                wd.ShowDialog();
+            });
+            OutputCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                OutputWindow wd = new OutputWindow();
+                wd.ShowDialog();
+            });
 
-    }
+        }
 
-    public bool IsLoaded { get => _isLoaded; set => _isLoaded = value; }
+        public bool IsLoaded { get => _isLoaded; set => _isLoaded = value; }
   }
 }
