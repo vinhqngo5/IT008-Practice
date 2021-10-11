e909bf6c684f543120d83bb7d84feccde34ffe6c
lehuutrung1412 <lehuutrung1412@users.noreply.github.com>
2021-10-11 09:02:27 +0700

    udpate viewmodel
    Signed-off-by: lehuutrung1412 <lehuutrung1412@users.noreply.github.com>

--- QuanLyKho/ViewModel/MainViewModel.cs
+++ QuanLyKho/ViewModel/MainViewModel.cs
@@ -4,24 +4,81 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace QuanLyKho.ViewModel
 {
-  class MainViewModel : BaseViewModel
-  {
-    private bool _isLoaded = false;
-    public MainViewModel()
+    class MainViewModel : BaseViewModel
     {
-      LoginWindow loginWindow = new LoginWindow();
+        public bool Isloaded = false;
+        public ICommand LoadedWindowCommand { get; set; }
+        public ICommand UnitWindowCommand { get; set; }
+        public ICommand SupplierWindowCommand { get; set; }
+        public ICommand CustomerWindowCommand { get; set; }
+        public ICommand ObjectWindowCommand { get; set; }
+        public ICommand UserWindowCommand { get; set; }
+        public ICommand InputWindowCommand { get; set; }
+        public ICommand OutputWindowCommand { get; set; }
 
-      if (!this.IsLoaded)
-      {
-        this.IsLoaded = true;
-        loginWindow.ShowDialog();
-      }
+        public MainViewModel()
+        {
+            LoadedWindowCommand = new RelayCommand<object>(
+                (p) => { return false; },
+                (p) =>
+                {
+                    Isloaded = true;
+                    LoginWindow loginWindow = new LoginWindow();
+                    loginWindow.ShowDialog();
+                });
 
+            UnitWindowCommand = new RelayCommand<object>(
+                (p) => { return true; },
+                (p) =>
+                {
+                    _ = new UnitWindow().ShowDialog();
+                });
+
+            SupplierWindowCommand = new RelayCommand<object>(
+                (p) => { return true; },
+                (p) =>
+                {
+                    _ = new SupplierWindow().ShowDialog();
+                });
+
+            CustomerWindowCommand = new RelayCommand<object>(
+                (p) => { return true; },
+                (p) =>
+                {
+                    _ = new CustomerWindow().ShowDialog();
+                });
+
+            ObjectWindowCommand = new RelayCommand<object>(
+                (p) => { return true; },
+                (p) =>
+                {
+                    _ = new ObjectWindow().ShowDialog();
+                });
+
+            UserWindowCommand = new RelayCommand<object>(
+                (p) => { return true; },
+                (p) =>
+                {
+                    _ = new UserWindow().ShowDialog();
+                });
+
+            InputWindowCommand = new RelayCommand<object>(
+                (p) => { return true; },
+                (p) =>
+                {
+                    _ = new InputWindow().ShowDialog();
+                });
+
+            OutputWindowCommand = new RelayCommand<object>(
+                (p) => { return true; },
+                (p) =>
+                {
+                    _ = new OutputWindow().ShowDialog();
+                });
+        }
     }
-
-    public bool IsLoaded { get => _isLoaded; set => _isLoaded = value; }
-  }
 }
