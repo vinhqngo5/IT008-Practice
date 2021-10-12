87ead34260d6f08c08d33968da5ea497a7f81e6b
19521178 <lthyntat>
2021-10-12 15:43:55 +0700

    Create DataProvider

---
+++ QuanLyKho/Model/DataProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyKho.Model
+{
+    public class DataProvider
+    {
+        private static DataProvider s_instance;
+
+        public static DataProvider Instance 
+        { 
+            get => s_instance ?? (s_instance = new DataProvider()); 
+            private set => s_instance = value; 
+        }
+        private DataProvider() 
+        {
+            DB = new QUANLYKHOEntities();
+        }
+        public QUANLYKHOEntities DB { get; set; }
+    }
+}
