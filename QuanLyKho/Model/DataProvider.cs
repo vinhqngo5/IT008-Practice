98a5902b1e0e78f55937448bc5395b72f2a01a72
lehuutrung1412 <lehuutrung1412@users.noreply.github.com>
2021-10-12 21:12:43 +0700

    model
    Signed-off-by: lehuutrung1412 <lehuutrung1412@users.noreply.github.com>

---
+++ QuanLyKho/Model/DataProvider.cs
@@ -0,0 +1,16 @@
+﻿namespace QuanLyKho.Model
+{
+    public class DataProvider
+    {
+        private DataProvider _instance;
+
+        public DataProvider Instance => _instance ?? (_instance = new DataProvider());
+
+        private DataProvider()
+        {
+            Database = new QUANLYKHOEntities();
+        }
+
+        public QUANLYKHOEntities Database { get; set; }
+    }
+}
